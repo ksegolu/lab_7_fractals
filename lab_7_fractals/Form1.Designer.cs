f3376cfb61d46a265ef8d02bfd79da55db16add9
Ксения Голубева <golubeva3.ka@edu.spbstu.ru>
2021-04-04 22:06:12 +0300

    Add project files.

---
+++ lab_7_fractals/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace lab_7_fractals
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(34, 115);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(389, 321);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(466, 101);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(504, 351);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1003, 542);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ lab_7_fractals/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace lab_7_fractals
+{
+    public partial class Form1 : Form
+    {
+        string filename;
+        Bitmap img_2;
+        //int cellSize;
+        int celllInLine;
+        int cellCount;
+        float[,,] u0, u, b;
+        int w, h;
+        const int delta = 4;
+        double[] xi = new double[delta];
+        double[] yi = new double[delta];
+        float[,] vol;
+        int q;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string path = "unh_4.jpg";
+            Image<Bgr, Byte> image = new Image<Bgr, Byte>(path);
+            Image<Bgr, Byte> image2 = image.Convert<Bgr, Byte>().Resize(1024, 1024, Emgu.CV.CvEnum.Inter.Linear);
+            image2.Save("edited.png");
+            Bitmap img_2 = new Bitmap("edited.png");
+            pictureBox1.Image = img_2;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            h = img_2.Height;
+            w = img_2.Width;
+            q = 0;
+            double[] res = new double[9];
+            for (int cellSize = 4; cellSize <= Math.Pow(2, 10); cellSize *= 2)
+            {
+                celllInLine = 1024 / cellSize;
+                cellCount = celllInLine * celllInLine;
+                vol = new float[cellCount, delta];
+                u0 = new float[cellCount, cellSize, cellSize];
+                u = new float[cellCount, cellSize, cellSize];
+                b = new float[cellCount, cellSize, cellSize];
+                setU0(img_2, cellSize);
+                float s;
+                //calc u and b 2 times
+                for (int d = 1; d <= delta; d++)
+                {
+                    calcU(d, cellSize);
+                    calcB(d, cellSize);
+                    for (int k = 0; k < cellCount; k++)
+                    {
+                        s = 0;
+                        for (int i = cellSize - 1; i >= 0; i--)
+                        {
+                            for (int j = cellSize - 1; j >= 0; j--)
+                            {
+                                s += u[k, i, j] - b[k, i, j];
+                            }
+                        }
+                        vol[k, d - 1] = s;
+                    }
+
+                }
+                s = 0;
+                for (int k = 0; k < cellCount; k++)
+                {
+                    s += vol[k, 0] / 2;
+                }
+                yi[0] = Math.Log(s);
+                xi[0] = Math.Log(1);
+                for (int d = 1; d < delta; d++)
+                {
+                    s = 0;
+                    for (int k = 0; k < cellCount; k++)
+                    {
+                        s += (vol[k, d] - vol[k, d - 1]) / 2;
+                    }
+                    yi[d] = Math.Log(s);
+                    xi[d] = Math.Log(d + 1);
+                }
+                res[q] = 2.0 + MNK(delta);
+                q++;
+            }
+
+        }
+
+
+
+        private void setU0(Bitmap img, int cellS)
+        {
+            for (int k = 0; k < cellCount; k++)
+            {
+                for (int i = cellS - 1; i >= 0; i--)
+                {
+                    for (int j = cellS - 1; j >= 0; j--)
+                    {
+                        u0[k, i, j] = getIntense(img.GetPixel(i + (cellS * (k % celllInLine)), j + (cellS * (k / celllInLine))));
+                    }
+                }
+            }
+
+            return;
+        }
+        //верхняя поверхность uδ(i,j)
+        private void calcU(int delta, int cellS)
+        {
+            if (delta > 1)
+            {
+                for (int k = 0; k < cellCount; k++)
+                {
+                    for (int i = cellS - 1; i >= 0; i--)
+                    {
+                        for (int j = cellS - 1; j >= 0; j--)
+                        {
+                            u0[k, i, j] = u[k, i, j];
+                        }
+                    }
+                }
+            }
+            for (int k = 0; k < cellCount; k++)
+            {
+                for (int i = cellS - 1; i >= 0; i--)
+                {
+                    for (int j = cellS - 1; j >= 0; j--)
+                    {
+                        u[k, i, j] = Math.Max(
+                            u0[k, i, j] + 1,
+                            Math.Max(
+                                Math.Max(
+                                    i > 0 ? u0[k, i - 1, j] : 0,
+                                    j > 0 ? u0[k, i, j - 1] : 0
+                                    ),
+                                Math.Max(
+                                    i < cellS - 1 ? u0[k, i + 1, j] : 0,
+                                    j < cellS - 1 ? u0[k, i, j + 1] : 0
+                                    )
+                                )
+                            );
+                    }
+                }
+            }
+            return;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calcB(int delta, int cellS)
+        {
+            if (delta > 1)
+            {
+                for (int k = 0; k < cellCount; k++)
+                {
+                    for (int i = cellS - 1; i >= 0; i--)
+                    {
+                        for (int j = cellS - 1; j >= 0; j--)
+                        {
+                            u0[k, i, j] = b[k, i, j];
+                        }
+                    }
+                }
+            }
+            for (int k = 0; k < cellCount; k++)
+            {
+                for (int i = cellS - 1; i >= 0; i--)
+                {
+                    for (int j = cellS - 1; j >= 0; j--)
+                    {
+                        b[k, i, j] = Math.Min(
+                            u0[k, i, j] - 1,
+                            Math.Min(
+                                Math.Min(
+                                    i > 0 ? u0[k, i - 1, j] : 256,
+                                    j > 0 ? u0[k, i, j - 1] : 256
+                                    ),
+                                Math.Min(
+                                    i < cellS - 1 ? u0[k, i + 1, j] : 256,
+                                    j < cellS - 1 ? u0[k, i, j + 1] : 256
+                                    )
+                                )
+                            );
+                    }
+                }
+            }
+            return;
+        }
+        
+
+
+        private double MNK(int n)
+        {
+            double sx = 0,
+                sx2 = 0,
+                sxy = 0,
+                sy = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sx += xi[i];
+                sx2 += xi[i] * xi[i];
+                sxy += xi[i] * yi[i];
+                sy += yi[i];
+            }
+            sx /= n;
+            sx2 /= n;
+            sxy /= n;
+            sy /= n;
+            return (sxy - sx * sy) / (sx2 - sx * sx);
+        }
+        private float getIntense(Color color)
+        {
+            return 255 - (color.R + color.G + color.B) / 3;
+        }
+    }
+}
+
